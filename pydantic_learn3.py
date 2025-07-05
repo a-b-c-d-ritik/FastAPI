9f8b1c91e083b3fb737edb81183d7d62213c9bb9
Ritik Kumar <143861433+a-b-c-d-ritik@users.noreply.github.com>
2025-07-05 23:28:32 +0530

    05/07/25

--- pydantic_learn3.py
+++ pydantic_learn3.py
@@ -1,14 +1,15 @@
-from pydantic import BaseModel,EmailStr,AnyUrl
-from typing import List,Dict,Optional
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import List,Dict,Optional,Annotated
 
 class Patient(BaseModel):
-    name: str
+   # name: str
+    name: Annotated[str,Field(max_length=30,title='Name of the patient',description='Give the name of patient under 50 words',examples=['Nitish','Raju'],default=None)]
     age: int
-    weight:float
+    weight:float=Field(gt=0) #gt mtlb greater than
     married:bool
     allergies:Optional[List[str]]=None
     email: EmailStr
-    linkedin_url:AnyUrl
+    linkedin_url: AnyUrl
 
 
 patient_info={
@@ -17,8 +18,8 @@ patient_info={
     'weight':78.45,
     'married':True,
     'allergies':['dust','smoke','webs'],
-    'email':'bakuraj@gmail.com'
-    'linkedin_url':'https//facebook.com'
+    'email':'bakuraj@gmail.com',
+    'linkedin_url':'https://www.perplexity.ai/'
     }
 
 p1=Patient(**patient_info)
